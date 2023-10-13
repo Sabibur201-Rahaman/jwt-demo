c2ae934799fa018c02239fae538a971f2a309b20
Sabibur201-Rahaman <107191991+Sabibur201-Rahaman@users.noreply.github.com>
2023-10-13 22:49:33 +0530

    token generated but not verified

--- src/controllers/appControllers.js
+++ src/controllers/appControllers.js
@@ -5,7 +5,7 @@ const home = (req, res) => {
   const tokenString = req.headers.authorization;
   const token = tokenString.split(' ')[1];
   console.log('token:::', token);
-  // jwtToken.verify(token,constant.secretkey,(error,decodedData)=>{  //this line has some issues
+  jwtToken.verify(token,constant.secretkey,(error,decodedData)=>{  //this line has some issues
     if(error){
       res.status(401).end('invalid data')
     }else{
@@ -14,8 +14,8 @@ const home = (req, res) => {
   })
 }
   const signin = (req, res) => {
-    // const userData = req.body.data;
-// console.log(userData)
+    const userData = req.body.data;
+console.log(userData)
     if (
       userData.email === "sabiburrahaman720@gmail.com" &&
       userData.password === 12345
